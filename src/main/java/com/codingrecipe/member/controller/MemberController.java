ea2c66d8e521fa5acbf6233078841cd4369a1d0e
codingrecipe1 <codingrecipe1@gmail.com>
2022-11-18 14:50:01 +0900

    13_로그아웃 및 완료

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -83,6 +83,12 @@ public class MemberController {
         return "redirect:/member/";
     }
 
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "index";
+    }
+
 }
 
 
